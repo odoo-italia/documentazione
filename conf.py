97c93668f65bf17084fe3148c41c0455c1107ed1
SimoRubi <simone.rubino@agilebg.com>
2019-03-10 12:45:42 +0100

    Draft

--- conf.py
+++ conf.py
@@ -80,8 +80,12 @@ html_theme = 'alabaster'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#
-# html_theme_options = {}
+
+html_theme_options = {
+    'logo': 'logo.png',
+    'github_user': 'odoo-italia',
+    'github_repo': 'documentazione',
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
